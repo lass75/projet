5112be3425e21141b14bab78b934ee67acae0c47
lass75 <149674910+lass75@users.noreply.github.com>
2025-05-23 19:38:25 +0200

    Delete Py directory

--- Py/nmap.py
+++
@@ -1,7 +0,0 @@
-from core.nmap_module import run_nmap_scan
-
-# Exemple d'IP locale ou cible à scanner
-target = "127.0.0.1"  # ou "scanme.nmap.org" pour tester en ligne
-
-result = run_nmap_scan(target)
-print(result)
